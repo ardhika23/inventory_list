ec2346c621cf398563ca5a53edf385e5b6ae5c32
ardhika23 <ardhika@gmail.com>
2023-09-22 15:10:40 +0700

    Finished assignment 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -7,3 +8,4 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
